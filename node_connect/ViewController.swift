4011a829b9568b5eaf124688588b4eb0c093fe80
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-07-29 13:22:19 +0900

    initial webview commit

--- node_connect/ViewController.swift
+++ node_connect/ViewController.swift
@@ -1,19 +1,28 @@
-//
-//  ViewController.swift
-//  node_connect
-//
-//  Created by YOONJONG on 2021/07/28.
-//
-
 import UIKit
-
-class ViewController: UIViewController {
-
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+//    var webView: WKWebView!
+    
+    @IBAction func clickRequestBtn(_ sender: Any) {
+        webView.evaluateJavaScript("changeBackgroundColor()", completionHandler: nil)
+    }
+    
+    override func loadView() {
+        print("loadView appeared")
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        
+        webView.uiDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad appeared")
+        let serverURL = URL(string: "http://127.0.0.1:3000")
+        let request = URLRequest(url: serverURL!)
+        webView.load(request)
     }
-
-
+    
 }
-
